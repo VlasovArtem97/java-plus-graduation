42d9240d7159ccb550658eeb57cec46a9753e0d7
VlasovArtem <Yukionn@yandex.ru>
2026-02-28 16:03:09 +0300

    4. Дипломный проект. 3 часть. fix ошибок № 1

--- stats/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApp.java
+++ stats/aggregator/src/main/java/ru/practicum/aggregator/AggregatorApp.java
@@ -2,10 +2,12 @@ package ru.practicum.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.practicum.aggregator.service.AggregationStarter;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AggregatorApp {
 
     public static void main(String[] args) {
