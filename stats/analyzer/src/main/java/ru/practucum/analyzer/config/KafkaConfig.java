3521a216a29ae4327335025b176f6ac1a9bf6d6e
VlasovArtem <Yukionn@yandex.ru>
2026-02-28 18:00:18 +0300

    4. Дипломный проект. 3 часть. fix ошибок № 5

--- stats/analyzer/src/main/java/ru/practucum/analyzer/config/KafkaConfig.java
+++ stats/analyzer/src/main/java/ru/practucum/analyzer/config/KafkaConfig.java
@@ -8,11 +8,13 @@ import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.practicum.avrodeserialization.EventSimilarDeserializer;
 import ru.practicum.avrodeserialization.UserActionDeserializer;
 
 import java.util.Properties;
 
+@Configuration
 public class KafkaConfig {
 
     @Value("${kafka.port}")
